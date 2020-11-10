84b07212db27e4fc5a750ee1d65c231dbcff0190
Thomas <awtpianka@gmail.com>
2020-11-10 15:08:02 -0500

    Add autonomous and clean up code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAutonomous.java
@@ -29,78 +29,49 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="Basic: Mecanum Drive", group="Iterative Opmode")
-public class BasicMecanumDrive extends OpMode
+/**
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all iterative OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@Autonomous(name="Basic: Autonomous", group="Linear Opmode")
+public class BasicAutonomous extends LinearOpMode
 {
     // Declare OpMode members
     private ElapsedTime runtime = new ElapsedTime();
     private HardwareMecanumDrive drive = new HardwareMecanumDrive();
 
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
     @Override
-    public void init() {
+    public void runOpMode() {
         telemetry.addData("Status", "Initializing");
         drive.initialize(hardwareMap);
         telemetry.addData("Status", "Initialized");
-    }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
-    @Override
-    public void init_loop() {
-    }
-
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
-    @Override
-    public void start() {
+        waitForStart();
         runtime.reset();
-    }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
-    @Override
-    public void loop() {
-        // Setup a variable for each drive wheel to save power level for telemetry
-        double frontLeftPower;
-        double frontRightPower;
-        double backLeftPower;
-        double backRightPower;
+        // Have robot drive forward for 2 seconds
+        if (opModeIsActive())
+        {
+            drive.setPower(-1.0, -1.0, -1.0, -1.0);
+            sleep(2000);
+            drive.setPower(0, 0, 0, 0);
+        }
 
-        // Calculate each mathematical component of power
-        double hypotenuse = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
-        double angle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
-        double rotation = gamepad1.right_stick_x;
-
-        // Calculate power for each wheel
-        frontLeftPower = Range.clip(hypotenuse * Math.sin(angle) - rotation, -1.0, 1.0);
-        frontRightPower = Range.clip(hypotenuse * Math.cos(angle) + rotation, -1.0, 1.0);
-        backLeftPower = Range.clip(hypotenuse * Math.cos(angle) - rotation, -1.0, 1.0);
-        backRightPower = Range.clip(hypotenuse * Math.sin(angle) + rotation, -1.0, 1.0);
-
-        // Send calculated power to wheels
-        drive.setPower(frontLeftPower, frontRightPower, backLeftPower, backRightPower);
-
-        // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "front left (%.2f), front right (%.2f), back left (%.2f), back right (%.2f)",
-                                                    frontLeftPower, frontRightPower, backLeftPower, backRightPower);
-    }
-
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
-    @Override
-    public void stop() {
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMecanumDrive.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class HardwareMecanumDrive
+{
+    public DcMotor frontLeft = null;
+    public DcMotor frontRight = null;
+    public DcMotor backLeft = null;
+    public DcMotor backRight = null;
+    private HardwareMap map = null;
+
+    public HardwareMecanumDrive()
+    {
+
+    }
+
+    public void initialize(HardwareMap hwMap)
+    {
+        // Save reference to map
+        map = hwMap;
+
+        // Initialize motors
+        frontLeft = map.get(DcMotor.class, "frontLeft");
+        frontRight = map.get(DcMotor.class, "frontRight");
+        backLeft = map.get(DcMotor.class, "backLeft");
+        backRight = map.get(DcMotor.class, "backRight");
+
+        // Set motor direction
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    public void setPower(double frontLeftPower, double frontRightPower, double backLeftPower, double backRightPower)
+    {
+        // Set motor power
+        frontLeft.setPower(frontLeftPower);
+        frontRight.setPower(frontRightPower);
+        backLeft.setPower(backLeftPower);
+        backRight.setPower(backRightPower);
+    }
+}
